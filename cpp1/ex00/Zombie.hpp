5f4e1e65230e4b0c3157cc5f0767f946f51d9e24
EndikaCo <endikacorreia@gmail.com>
2022-06-06 14:37:31 +0200

    cpp1 ex01

---
+++ cpp1/ex00/Zombie.hpp
@@ -0,0 +1,25 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <string>
+
+class Zombie
+{
+private:
+    std::string name;
+public:
+    Zombie(/* args */); // Declare constructor
+    ~Zombie();          // Declare Destructor
+};
+
+Zombie::Zombie(/* args */)
+{
+}
+
+Zombie::~Zombie()
+{
+}
+
+
+
+#endif

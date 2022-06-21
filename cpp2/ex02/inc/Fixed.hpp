92b161197495290156bbbbb5a0519e4af01965e1
EndikaCo <endikacorreia@gmail.com>
2022-06-21 12:52:25 +0200

    las 2 value fail

--- cpp2/ex02/inc/Fixed.hpp
+++ cpp2/ex02/inc/Fixed.hpp
@@ -15,11 +15,35 @@ public:
     Fixed(const float par);
     ~Fixed();
     Fixed(Fixed const &orig);
+
+    bool operator>(Fixed const &a);
+    bool operator<(Fixed const &a);
+    bool operator<=(Fixed const &a);
+    bool operator>=(Fixed const &a);
+    bool operator==(Fixed const &a);
+    bool operator!=(Fixed const &a);
+
+    Fixed operator+(Fixed const &a)const;
+    Fixed operator-(Fixed const &a)const;
+    Fixed operator*(Fixed const &a)const;
+    Fixed operator/(Fixed const &a)const;
+
     Fixed &operator=(Fixed const &a);
+
+    Fixed operator++();
+    Fixed operator++(int);
+    Fixed operator--();
+    Fixed operator--(int);
+
     int getRawBits( void ) const; 
     void setRawBits( int const raw ); 
     float toFloat( void ) const;
     int toInt( void ) const;
+
+    static Fixed& min(Fixed &a, Fixed  &b);
+    static const Fixed&  min( const Fixed  &a, const Fixed  &b);
+    static Fixed& max(Fixed &a, Fixed  &b);
+    static const Fixed&  max(Fixed const &a, Fixed const &b);
 };
 
 std::ostream& operator<<(std::ostream& output, const Fixed& fixedPoint);
--- cpp2/ex02/src/Fixed.cpp
+++ cpp2/ex02/src/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: ecorreia <ecorreia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 09:51:06 by ecorreia          #+#    #+#             */
-/*   Updated: 2022/06/20 11:39:27 by ecorreia         ###   ########.fr       */
+/*   Updated: 2022/06/21 12:49:16 by ecorreia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 // A default constructor that initializes the fixed point value to 0.
 Fixed::Fixed() : fixedPoint(0)
 {
-    std::cout << "Default constructor called\n";
+    //std::cout << "Default constructor called\n";
 }
 
 /*A constructor that takes a constant integer as a parameter and that
@@ -24,7 +24,7 @@ converts it to the correspondant fixed(8) point value*/
 Fixed::Fixed(const int par)
 {
     fixedPoint = par << FRACTIONAL_BITS;
-    std::cout << "Int constructor called\n";   
+    //std::cout << "Int constructor called\n";   
     return;
 }
 
@@ -33,37 +33,121 @@ it to the correspondant fixed(8) point value.*/
 Fixed::Fixed(const float par)
 {
     fixedPoint = roundf(par * (1 << FRACTIONAL_BITS));
-    std::cout << "Float constructor called\n";
+    //std::cout << "Float constructor called\n";
 }
 
 //A destructor.
 Fixed::~Fixed()
 {
-    std::cout << "Destructor called\n";
+    //std::cout << "Destructor called\n";
 }
 
 //A copy constructor.
 Fixed::Fixed(Fixed const &orig)
 {
     *this = orig;
-    std::cout << "Copy constructor called\n";
+    //std::cout << "Copy constructor called\n";
 	return ;
+}
+
+/*COMPARISON OVERLOAD OPERATORS*/
+//comparison overload operator >
+bool Fixed::operator>(Fixed const &a)
+{
+    return (fixedPoint > a.fixedPoint);
+}
+//comparison overload operator <
+bool Fixed::operator<(Fixed const &a)
+{
+    return (fixedPoint < a.fixedPoint);
+}
+//comparison overload operator <=
+bool Fixed::operator<=(Fixed const &a)
+{
+    return (fixedPoint <= a.fixedPoint);
+}
+//comparison overload operator >=
+bool Fixed::operator>=(Fixed const &a)
+{
+    return (fixedPoint >= a.fixedPoint); 
+}
+//comparison overload operator ==
+bool Fixed::operator==(Fixed const &a)
+{
+    return (fixedPoint == a.fixedPoint); 
+}
+//comparison overload operator !=
+bool Fixed::operator!=(Fixed const &a)
+{
+    return (fixedPoint != a.fixedPoint); 
 }
 
 //An assignation operator overload.
 Fixed& Fixed::operator=(Fixed const& a)
 {
-    std::cout << "Assignation operator called\n";
+    //std::cout << "Assignation operator called\n";
     this->fixedPoint = a.getRawBits();
     return (*this);
 }
 
-/*An overload to the « operator that inserts a floating point representation of the
-fixed point value into the parameter output stream.*/
-std::ostream& operator<<(std::ostream& output, const Fixed& fixedP)
+/*ARITMETHIC OPERATORS*/
+//arithmetic overloads operators +
+Fixed Fixed::operator+(Fixed const &a) const
 {
-	output << fixedP.toFloat();
-	return (output);
+    Fixed fixed;
+     fixed = fixedPoint + a.fixedPoint;
+    return (fixed); 
+}
+//arithmetic overloads operators -
+Fixed Fixed::operator-(Fixed const &a) const
+{
+    Fixed fixed;
+    fixed = fixedPoint - a.fixedPoint;
+    return (fixed);    
+}
+//arithmetic overloads operators *
+Fixed Fixed::operator*(Fixed const &a) const
+{
+    Fixed fixed;
+    fixed = fixedPoint * a.fixedPoint;
+    return (fixed); 
+}
+//arithmetic overloads operators /
+Fixed Fixed::operator/(Fixed const &a) const
+{
+    Fixed fixed;
+    fixed = fixedPoint / a.fixedPoint;
+    return (fixed);     
+}
+
+/*INCREMENT OPERATORS*/
+//pre increment overload operator "++i"
+Fixed Fixed::operator++() 
+{
+    fixedPoint += 1;
+    return (*this);
+}
+//post increment overload operator "i++"
+Fixed Fixed::operator++(int) 
+{
+    Fixed cpy(*this);
+	operator++();
+    return (cpy);
+}
+
+/*DECREMENT OPERATORS*/
+//pre increment overload operator "--i"
+Fixed Fixed::operator--()
+{
+    fixedPoint -= 1;
+    return (*this);
+}
+//post increment overload operator "i--"
+Fixed Fixed::operator--(int)
+{
+    Fixed cpy(*this);
+	operator--();
+    return (cpy);
 }
 
 //A member function that returns the raw value of the fixed point value.
@@ -96,5 +180,43 @@ int Fixed::toInt( void ) const
     
 }
 
+/*The static member function min that takes references on two fixed point values and
+returns a reference to the smallest value,*/
 
-    
+Fixed& Fixed::min(Fixed &a, Fixed  &b)
+{
+    return (a < b ? a : b);
+}
+
+/* and an overload that takes references on
+two constant fixed point values and returns a reference to the smallest constant
+value.*/
+const Fixed& Fixed::min(  Fixed const &a,  Fixed const &b)
+{
+    return (a.getRawBits() < b.getRawBits() ? a : b);
+}
+
+/* The static member function max that takes references on two fixed point values
+and returns a reference to the biggest value, */
+
+Fixed& Fixed::max(Fixed &a, Fixed  &b)
+{
+    return (a > b ? a : b);
+}
+
+/*and an overload that takes references
+on two constant fixed point values and returns a reference to the biggest constant
+value.*/ 
+
+const Fixed& Fixed::max(Fixed const &a, Fixed const &b)
+{
+    return (a.getRawBits() > b.getRawBits() ? a : b);
+}
+
+/*An overload to the « operator that inserts a floating point representation of the
+fixed point value into the parameter output stream.*/
+std::ostream& operator<<(std::ostream& output, const Fixed& fixedP)
+{
+	output << fixedP.toFloat();
+	return (output);
+}

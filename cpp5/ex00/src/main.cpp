acb984b01d3fe7dbb14d3aa491338dcb6ce09e9e
EndikaCo <endikacorreia@gmail.com>
2022-10-26 14:08:13 +0200

    cpp5 ex00

---
+++ cpp5/ex00/src/main.cpp
@@ -0,0 +1,33 @@
+#include "../inc/Bureaucrat.hpp"
+
+int main() {
+    std::cout << "TEST1"<< std::endl;
+    Bureaucrat *b1;
+    try
+    {
+        b1 = new Bureaucrat();
+        std::cout << *b1 << std::endl;
+        b1->gradeUp();
+        b1->gradeDown();
+        b1->gradeDown();
+    }
+    catch(std::exception & e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    delete b1;
+
+    std::cout << "TEST2"<< std::endl;
+    Bureaucrat *b2;
+    try
+    {
+        b2 = new Bureaucrat("manolo", 1);
+        std::cout << *b2 << std::endl;
+        b2->gradeUp();
+    }
+    catch(std::exception & e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    delete b2;
+}
